97e7a2849dc8c27eec0f55ea90ec10c526fc453e
Jeremiah Xing <dev.jrmh@gmail.com>
2025-09-25 03:19:26 +1000

    fix Cannot find module or type declarations for global.css

---
+++ globals.d.ts
@@ -0,0 +1,21 @@
+// Global type declarations for CSS and other asset imports
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
